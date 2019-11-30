06d1ca2b0e8a4c1e28c90c8747e47412cde24483
KienLee0207 <46680892+KienLee0207@users.noreply.github.com>
2019-11-30 08:12:17 +0700

    up
    up

---
+++ HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThuVienApp.Models;
+
+namespace ThuVienApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            List<DataPoint> dataPoints1 = new List<DataPoint>();
+			List<DataPoint> dataPoints2 = new List<DataPoint>();
+			List<DataPoint> dataPoints3 = new List<DataPoint>();
+			List<DataPoint> dataPoints4 = new List<DataPoint>();
+ 
+			dataPoints1.Add(new DataPoint(1483209000000, 65));
+			dataPoints1.Add(new DataPoint(1485887400000, 62));
+			dataPoints1.Add(new DataPoint(1488306600000, 65));
+			dataPoints1.Add(new DataPoint(1490985000000, 20));
+			dataPoints1.Add(new DataPoint(1493577000000, 36));
+			dataPoints1.Add(new DataPoint(1496255400000, 53));
+ 
+			dataPoints2.Add(new DataPoint(1483209000000, 85));
+			dataPoints2.Add(new DataPoint(1485887400000, 36));
+			dataPoints2.Add(new DataPoint(1488306600000, 15));
+			dataPoints2.Add(new DataPoint(1490985000000, 25));
+			dataPoints2.Add(new DataPoint(1493577000000, 65));
+			dataPoints2.Add(new DataPoint(1496255400000, 49));
+ 
+			dataPoints3.Add(new DataPoint(1483209000000, 63));
+			dataPoints3.Add(new DataPoint(1485887400000, 46));
+			dataPoints3.Add(new DataPoint(1488306600000, 21));
+			dataPoints3.Add(new DataPoint(1490985000000, 55));
+			dataPoints3.Add(new DataPoint(1493577000000, 46));
+			dataPoints3.Add(new DataPoint(1496255400000, 50));
+ 
+			dataPoints4.Add(new DataPoint(1483209000000, 45));
+			dataPoints4.Add(new DataPoint(1485887400000, 55));
+			dataPoints4.Add(new DataPoint(1488306600000, 33));
+			dataPoints4.Add(new DataPoint(1490985000000, 60));
+			dataPoints4.Add(new DataPoint(1493577000000, 39));
+			dataPoints4.Add(new DataPoint(1496255400000, 70));
+ 
+			ViewBag.DataPoints1 = JsonConvert.SerializeObject(dataPoints1);
+			ViewBag.DataPoints2 = JsonConvert.SerializeObject(dataPoints2);
+			ViewBag.DataPoints3 = JsonConvert.SerializeObject(dataPoints3);
+			ViewBag.DataPoints4 = JsonConvert.SerializeObject(dataPoints4);
+ 
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SachesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ThuVienApp;
+
+namespace ThuVienApp.Controllers
+{
+    public class SachesController : Controller
+    {
+        private SampleDataEntities db = new SampleDataEntities();
+
+        // GET: Saches
+        public ActionResult Index()
+        {
+            var sachs = db.Saches.Include(c => c.Ten_SH).OrderByDescending(o => o.id_SH);
+            return View(db.Saches.ToList());
+        }
+
+        // GET: Saches/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sach sach = db.Saches.Find(id);
+            if (sach == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sach);
+        }
+
+        // GET: Saches/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Saches/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_SH,TheLoai,Ten_SH,TacGia,SoLuong,Created_at,Deleted_at")] Sach sach)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Saches.Add(sach);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sach);
+        }
+
+        // GET: Saches/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sach sach = db.Saches.Find(id);
+            if (sach == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sach);
+        }
+
+        // POST: Saches/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_SH,TheLoai,Ten_SH,TacGia,SoLuong,Created_at,Deleted_at")] Sach sach)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sach).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sach);
+        }
+
+        // GET: Saches/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sach sach = db.Saches.Find(id);
+            if (sach == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sach);
+        }
+
+        // POST: Saches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sach sach = db.Saches.Find(id);
+            db.Saches.Remove(sach);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        [HttpPost]
+        public ActionResult Search(string sach, string tacgia)
+        {
+            var sachs = db.Saches.Where(Search => Search.Ten_SH == sach)
+                .OrderByDescending(o => o.id_SH);
+            var tacgias = db.Saches.Where(Search => Search.TacGia == tacgia)
+                .OrderByDescending(o => o.id_SH);
+            return View(sachs);
+        }
+
+    }
+}
